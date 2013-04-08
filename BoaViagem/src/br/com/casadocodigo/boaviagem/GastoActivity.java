5e39feae9d2dd22bd253d659f95c4018ef482622
Rodrigo Almeida <dgrodrigo@gmail.com>
2013-04-08 00:51:43 -0300

    Implementando primeiro datePicker com listener

--- BoaViagem/src/br/com/casadocodigo/boaviagem/GastoActivity.java
+++ BoaViagem/src/br/com/casadocodigo/boaviagem/GastoActivity.java
@@ -2,26 +2,42 @@ package br.com.casadocodigo.boaviagem;
 
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 public class GastoActivity extends Activity{
 
+    private int ano,mes,dia;
     private Button dataGasto;
+
+    private DatePickerDialog.OnDateSetListener listener = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker datePicker, int year, int monthOfYear, int dayOfMonth) {
+            ano = year;
+            mes = monthOfYear;
+            dia = dayOfMonth;
+            dataGasto.setText(dia + "/" + (mes+1) + "/" + ano);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.gasto);
-        SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
         Calendar calendar = Calendar.getInstance();
+        ano = calendar.get(Calendar.YEAR);
+        mes = calendar.get(Calendar.MONTH);
+        dia = calendar.get(Calendar.DAY_OF_MONTH);
 
         dataGasto = (Button) findViewById(R.id.data);
-        dataGasto.setText(formato.format(calendar.getTime()));
+        dataGasto.setText(dia + "/" + (mes+1) + "/" + ano);
 
     }
 
@@ -29,4 +45,13 @@ public class GastoActivity extends Activity{
         showDialog(view.getId());
     }
 
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        if(R.id.data == id) {
+            return new DatePickerDialog(this,listener, ano,mes,dia);
+        }
+
+        return null;
+    }
 }
